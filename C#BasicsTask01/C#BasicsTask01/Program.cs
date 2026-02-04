6e5c8208763a888a9cb056e25965af89eb694f4a
Basma516 <basmaelmenshawy3@gmail.com>
2026-02-04 23:41:00 +0200

    save first assignment

--- C#BasicsTask01/C#BasicsTask01/Program.cs
+++ C#BasicsTask01/C#BasicsTask01/Program.cs
@@ -6,90 +6,90 @@
         {
 
 
-            //Console.WriteLine("================find the largest of three numbers================");
-            //Console.Write("Enter first number: ");
-            //int num1 = Convert.ToInt32(Console.ReadLine());
-            //Console.Write("Enter second number: ");
-            //int num2 = Convert.ToInt32(Console.ReadLine());
-            //Console.Write("Enter third number: ");
-            //int num3 = Convert.ToInt32(Console.ReadLine());
-            //int largest;
-            //if (num1 >= num2 && num1 >= num3)
-            //{
-            //    largest = num1;
-            //}
-            //else if (num2 >= num1 && num2 >= num3)
-            //{
-            //    largest = num2;
-            //}
-            //else
-            //{
-            //    largest = num3;
-            //}
-            //Console.WriteLine($"The largest number is: {largest}");
-            //Console.WriteLine("=================================================================");
+            Console.WriteLine("================find the largest of three numbers================");
+            Console.Write("Enter first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter third number: ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            int largest;
+            if (num1 >= num2 && num1 >= num3)
+            {
+                largest = num1;
+            }
+            else if (num2 >= num1 && num2 >= num3)
+            {
+                largest = num2;
+            }
+            else
+            {
+                largest = num3;
+            }
+            Console.WriteLine($"The largest number is: {largest}");
+            Console.WriteLine("=================================================================");
 
-            //Console.WriteLine("================find the largest of three numbers by using lists================");
-            //List<int> numbers = new List<int>();
-            //Console.Write("Enter first number: ");
-            //numbers.Add(Convert.ToInt32(Console.ReadLine()));
-            //Console.Write("Enter second number: ");
-            //numbers.Add(Convert.ToInt32(Console.ReadLine()));
-            //Console.Write("Enter third number: ");
-            //numbers.Add(Convert.ToInt32(Console.ReadLine()));
-            //int largest = numbers.Max();
-            //Console.WriteLine($"The largest number is: {largest}");
-            //Console.WriteLine("=================================================================");
+            Console.WriteLine("================find the largest of three numbers by using lists================");
+            List<int> numbers = new List<int>();
+            Console.Write("Enter first number: ");
+            numbers.Add(Convert.ToInt32(Console.ReadLine()));
+            Console.Write("Enter second number: ");
+            numbers.Add(Convert.ToInt32(Console.ReadLine()));
+            Console.Write("Enter third number: ");
+            numbers.Add(Convert.ToInt32(Console.ReadLine()));
+            int largestNum = numbers.Max();
+            Console.WriteLine($"The largest number is: {largestNum}");
+            Console.WriteLine("=================================================================");
 
-            //Console.WriteLine("============Determining the type of triangle============");
-            //Console.Write("Enter length of first side: ");
-            //int side1 = Convert.ToInt32(Console.ReadLine());
-            //Console.Write("Enter length of second side: ");
-            //int side2 = Convert.ToInt32(Console.ReadLine());
-            //Console.Write("Enter length of third side: ");
-            //int side3 = Convert.ToInt32(Console.ReadLine());
-            //if (side1 == side2 && side2 == side3)
-            //{
-            //    Console.WriteLine("The triangle is equilateral.");
-            //}
-            //else if (side1 == side2 || side2 == side3 || side1 == side3)
-            //{
-            //    Console.WriteLine("The triangle is isosceles.");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("The triangle is scalene.");
-            //}
-            //Console.WriteLine("=================================================================");
-            //Console.WriteLine("========== read 10 numbers and find thier avg and sum============ ");
-            //int[] arr = new int[10];
-            //int sum = 0;
-            //for (int i = 0; i < arr.Length; i++)
-            //{
-            //    Console.Write($"Enter number {i+1}: ");
-            //    arr[i] = Convert.ToInt32(Console.ReadLine());
-            //    sum += arr[i] ;
-            //}
-            //double average = sum / arr.Length;
-            //Console.WriteLine($"Sum of the Numbers is {sum} and thier Average is {average}.");
-            //Console.WriteLine("=================================================================");
+            Console.WriteLine("============Determining the type of triangle============");
+            Console.Write("Enter length of first side: ");
+            int side1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter length of second side: ");
+            int side2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter length of third side: ");
+            int side3 = Convert.ToInt32(Console.ReadLine());
+            if (side1 == side2 && side2 == side3)
+            {
+                Console.WriteLine("The triangle is equilateral.");
+            }
+            else if (side1 == side2 || side2 == side3 || side1 == side3)
+            {
+                Console.WriteLine("The triangle is isosceles.");
+            }
+            else
+            {
+                Console.WriteLine("The triangle is scalene.");
+            }
+            Console.WriteLine("=================================================================");
+            Console.WriteLine("========== read 10 numbers and find thier avg and sum============ ");
+            int[] arr = new int[10];
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"Enter number {i + 1}: ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+                sum += arr[i];
+            }
+            double average = sum / arr.Length;
+            Console.WriteLine($"Sum of the Numbers is {sum} and thier Average is {average}.");
+            Console.WriteLine("=================================================================");
 
-            //Console.WriteLine("============Display the multiplication table of the given number============");
-            //Console.Write("Enter a number to display its multiplication table: ");
-            //int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("============Display the multiplication table of the given number============");
+            Console.Write("Enter a number to display its multiplication table: ");
+            int number = Convert.ToInt32(Console.ReadLine());
 
-            //Console.WriteLine($"Multiplication table of {number}:");
-            //for (int i = 0; i < number; i++)
-            //{
-            //    Console.WriteLine($"{number} x {i + 1} = {number * (i + 1)}");
-            //}
+            Console.WriteLine($"Multiplication table of {number}:");
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine($"{number} x {i + 1} = {number * (i + 1)}");
+            }
 
-            //Console.WriteLine("=================================================================");
-            //Console.WriteLine("=======Factorial of the given number by using Recursion function");
-            //Console.Write("Enter a number to calc Factorial:");
-            //int num = Convert.ToInt32(Console.ReadLine());
-            //int fact = Factorial(num);
-            //Console.WriteLine($"Factorial of {num} is {fact}.");
+            Console.WriteLine("=================================================================");
+            Console.WriteLine("=======Factorial of the given number by using Recursion function");
+            Console.Write("Enter a number to calc Factorial:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int fact = Factorial(num);
+            Console.WriteLine($"Factorial of {num} is {fact}.");
             Console.WriteLine("=================================================================");
             while (true)
             {
