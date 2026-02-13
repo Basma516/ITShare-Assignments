d2cc3983fca52f72b541b5e77224794ee5d294af
Basma516 <basmaelmenshawy3@gmail.com>
2026-02-13 08:47:13 +0200

    add task 5

---
+++ C#session5/C#session5/IMovable.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_session5
+{
+    public interface IMovable
+    {
+        void MoveUp();
+        void MoveDown();
+        void MoveLeft();
+        void MoveRight();
+    }
+    public class MovablePoint : IMovable
+    {
+        internal int x;
+        internal int y;
+        internal int xSpeed;
+        internal int ySpeed;
+        public MovablePoint(int x, int y, int xSpeed, int ySpeed)
+        {
+            this.x = x;
+            this.y = y;
+            this.xSpeed = xSpeed;
+            this.ySpeed = ySpeed;
+        }
+        public void MoveUp()
+        {
+            y += ySpeed;
+        }
+        public void MoveDown()
+        {
+            y -= ySpeed;
+        }
+        public void MoveLeft()
+        {
+            x -= xSpeed;
+        }
+        public void MoveRight()
+        {
+            x += xSpeed;
+        }
+        public override string ToString()
+        {
+            return "MovablePoint: (" + x + "," + y + ") speed=(" + xSpeed + "," + ySpeed + ")";
+        }
+    }
+}
---
+++ C#session5/C#session5/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C_session5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
